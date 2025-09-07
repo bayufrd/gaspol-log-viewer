5436964314104f1d0fa6d3fcc63dc89f4ebfe094
bayufrd <55101246+bayufrd@users.noreply.github.com>
2025-09-07 23:59:10 +0700

    [UPDATE] adding fiture copy error on logs windows entry

--- components/LogWindowsEntry.js
+++ components/LogWindowsEntry.js
@@ -21,17 +21,38 @@ const LogWindowsEntry = ({
 
     const { bg, text } = levelColors[level] || levelColors['info'];
 
+    const handleCopy = () => {
+        const logText = `
+            ${visibleFields?.log_level ? `${level.toUpperCase()} [${logCode}]` : ''}
+            ${visibleFields?.outlet_id ? `⚠️ Outlet ID: ${outlet_id} (${outletName})` : ''}
+            ${visibleFields?.message ? `⚠️ msg: ${message}` : ''}
+            ${visibleFields?.exception ? `⚠️ ex: ${exception}` : ''}
+            ${visibleFields?.source ? `⚠️ sc: ${source}` : ''}
+            ${visibleFields?.additional_info ? `⚠️ adtinfo: ${additionalInfo}` : ''}
+        `.trim();
+
+        navigator.clipboard.writeText(logText)
+            .then(() => alert('Log entry copied to clipboard!'))
+            .catch(err => console.error('Error copying log entry:', err));
+    };
+
     return (
         <div className={`log-entry p-2 mb-2 rounded ${bg}`}>
             <div className="d-flex justify-content-between align-items-center mb-1" style={{ fontFamily: 'Courier New, Courier, monospace' }}>
-                {visibleFields?.created_at && (
-                    <small className="text-white-300">{timestamp}</small>
-                )}
-                {visibleFields?.log_level && (
-                    <span className={`badge ${text} text-uppercase`}>
-                        {level.toUpperCase()}
-                    </span>
-                )}
+                <div>
+                    {visibleFields?.created_at && (
+                        <small className="text-white-300">{timestamp}</small>
+                    )}
+                    {visibleFields?.log_level && (
+                        <span className={`badge ${text} text-uppercase`}>
+                            {level.toUpperCase()}
+                        </span>
+                    )}
+                </div>
+                {/* Tombol Salin */}
+                <button className="btn btn-outline-light btn-sm" onClick={handleCopy}>
+                    Copy
+                </button>
             </div>
 
             <div
